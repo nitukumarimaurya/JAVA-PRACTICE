f8873e32c7a30e2fbf626bed5670f83754429be5
Lenovo <Lenovo@DESKTOP-V3NU27T>
2021-08-07 14:47:03 +0530

    Iniial commit

---
+++ Array/src/ademo.java
@@ -0,0 +1,9 @@
+
+public class ademo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Array/src/array/ademo.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.Arrays;
+
+public class ademo 
+{
+	public static void main(String args[])
+	{
+		int[] num=new int[6];
+		num[0]=1;
+		num[1]=2;
+		num[4]=3;
+	    //Arrays.toString(num);
+		System.out.println(num.length);
+		System.out.println( Arrays.toString(num));
+		
+		int[] numbers= {1,4,3,6,7,8};
+		System.out.println( Arrays.toString(numbers));
+	}
+
+}
